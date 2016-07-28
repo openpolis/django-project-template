2ebb6c4e657d5bd0d6323bdb04afe3c391a32cae
guglielmo <guglielmo.celata@gmail.com>
2016-07-29 01:20:13 +0200

    debug toolbar is included only if required in .env

--- {{cookiecutter.repo_name}}/project/{{cookiecutter.package_name}}/settings/development.py
+++ {{cookiecutter.repo_name}}/project/{{cookiecutter.package_name}}/settings/development.py
@@ -31,20 +31,20 @@ DATABASES = {
 
 ########## TOOLBAR CONFIGURATION
 # See: http://django-debug-toolbar.readthedocs.org/en/latest/installation.html#explicit-setup
-INSTALLED_APPS += (
-    'debug_toolbar',
-)
+if DEBUG_TOOLBAR:
+    INSTALLED_APPS += (
+        'debug_toolbar',
+    )
 
-MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
+    MIDDLEWARE_CLASSES += (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    )
 
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
+    DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
-# http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
-INTERNAL_IPS = ('127.0.0.1',)
+    # http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
+    INTERNAL_IPS = ('127.0.0.1',)
 ########## END TOOLBAR CONFIGURATION
-
 
 ########## LOGGING CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#logging
