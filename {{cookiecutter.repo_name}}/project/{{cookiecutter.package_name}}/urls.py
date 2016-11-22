2940c5ff37230a0dc503412f29d7bff23e8efd83
guglielmo <guglielmo.celata@gmail.com>
2016-11-22 13:57:27 +0100

    bug on url.pattern corrected

--- {{cookiecutter.repo_name}}/project/{{cookiecutter.package_name}}/urls.py
+++ {{cookiecutter.repo_name}}/project/{{cookiecutter.package_name}}/urls.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
