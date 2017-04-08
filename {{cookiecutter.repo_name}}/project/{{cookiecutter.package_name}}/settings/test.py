fb6138e25c7cea1967a4be28abaff3bc222b8bfc
Guglielmo Celata <guglielmo.celata@gmail.com>
2017-04-08 06:59:11 +0200

    Update test.py

--- {{cookiecutter.repo_name}}/project/{{cookiecutter.package_name}}/settings/test.py
+++ {{cookiecutter.repo_name}}/project/{{cookiecutter.package_name}}/settings/test.py
@@ -26,6 +26,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#logging
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
+LOGGING['handlers']['console'] = {
+    'level': 'DEBUG',
+    'class': 'logging.StreamHandler',
+    'formatter': 'verbose',
+}
 LOGGING['loggers'] = {
     '': {
         'handlers': ['console'],
